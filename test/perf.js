a6fb8f033df5d99dca8e398b09821e9edd131165
Dmitry Usa <atskiisotona@gmail.com>
2015-10-06 14:58:09 +0800

    Update perf test

--- test/perf.js
+++ test/perf.js
@@ -1,7 +1,12 @@
 var fs = require("fs");
 var p = require("../protobuf");
+var pjs = require("protobufjs")
 var desc = fs.readFileSync(__dirname + "/points.desc");
+var pbjsBuilder = pjs.loadProtoFile(__dirname + "/points.proto")
+
 var pb = new p(desc);
+var pointsBuilder = pbjsBuilder.build("Points")
+var point = pbjsBuilder.build("Point")
 
 var points = [];
 for (var i = 0; i < 100000; i++) {
@@ -10,6 +15,12 @@ for (var i = 0; i < 100000; i++) {
 		j: 1388534400000
 	});
 }
+
+var par = []
+for (var i = 0; i < 100000; i++) {
+	par.push(new point(1388534400000, 1388534400000))
+}
+var pointsjs = new pointsBuilder(par)
 
 /*var t1 = process.hrtime()
 
@@ -26,11 +37,23 @@ var diff = t2[0] * 1e9 + t2[1] - (t1[0] * 1e9 + t1[1])
 console.log("Time JSON:", diff / 1000);*/
 
 var t1 = process.hrtime()
-var serPB = pb.serialize({
-	points: points
-}, 'Points');
-var deserPB = pb.parse(serPB, 'Points');
+for (var i = 0; i < 10; i++) {
+	var serPB = pb.serialize({
+		points: points
+	}, 'Points');
+	var deserPB = pb.parse(serPB, 'Points');
+}
 var t2 = process.hrtime()
 
 var diff = t2[0] * 1e9 + t2[1] - (t1[0] * 1e9 + t1[1])
-console.log("  Time PB:", diff);
+console.log("  Time PB:", diff / 10);
+
+var t1 = process.hrtime()
+for (var i = 0; i < 10; i++) {
+	var serPB = pointsjs.encode()
+	var deserPB = pointsBuilder.decode(serPB)
+}
+var t2 = process.hrtime()
+
+var diff = t2[0] * 1e9 + t2[1] - (t1[0] * 1e9 + t1[1])
+console.log("Time PBJS:", diff / 10);
